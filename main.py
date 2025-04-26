bb437a4dff3967ccf8b552acedda480647c159c3
Walter Camargo Neto <162071818+WC-Neto@users.noreply.github.com>
2025-04-26 18:31:39 -0400

    CLI base
    Acrescentei uma CLI básica, defini o atributo categoria para pesquisa no dicionário.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#Inicio
+import regras
 class Atividades:
-    def __init__(self, nome: str, horas_reais: int, peso: float):
+    def __init__(self, nome: str,categoria: str, horas_reais: int, peso: float):
         self._nome = nome
+        self._categoria = categoria
         self.horas_reais = horas_reais
         self._peso = peso
 
@@ -9,36 +10,41 @@ class Atividades:
     def nome(self):
         return self._nome
 
-    @nome.setter
-    def nome(self, value):
-        self._nome = value
-
+    @property
+    def categoria(self):
+        return self._categoria
+        
     @property
     def horas_reais(self):
         return self._horas_reais
-
-    @horas_reais.setter
-    def horas_reais(self, value):
-        if value < 0:
-            raise ValueError("Horas não podem ser negativas!")
-        self._horas_reais = value
-        
-        
-
+    
     @property
     def peso(self):
         return self._peso
 
+    @horas_reais.setter
+    def horas_reais(self, value):
+        if value <= 0:
+            raise ValueError("Horas não podem ser negativas!")
+        self._horas_reais = value
+
     @peso.setter
     def peso(self, value):
+        if value <= 0:
+            raise ValueError("Peso não pode ser negativas!")    
         self._peso = value
-
+        
     @property
     def horas_validas(self):
         return self.horas_reais * self.peso
 
 
-teste = Atividades("Filme", -12 , 2.0)
-print(teste.horas_validas)
-
+teste = Atividades("Filme", "Cultura", 12 , 2.0)
+menu = input('''
+      Bem vindo ao ExtraIF!
+      Espero te ajudar a validar suas horas sem estresse!
+      Qual seu tipo de curso?
+      [1] Superior
+      [2] Técnico
+      ''')
 
